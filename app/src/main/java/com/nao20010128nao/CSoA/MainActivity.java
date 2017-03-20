2f5603a01739985e02530c37ecd3cc176c7a08bc
nao20010128nao <nao20010128@gmail.com>
2017-03-21 00:11:40 +0900

    最初

---
+++ app/src/main/java/com/nao20010128nao/CSoA/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nao20010128nao.CSoA;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
