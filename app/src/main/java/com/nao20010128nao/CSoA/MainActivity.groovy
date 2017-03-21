69de03abb2ac3bdea567330e965fbf841a41dc72
nao20010128nao <nao20010128@gmail.com>
2017-03-21 15:01:19 +0900

    動的クラス生成&実行可能に Big thanks for melix/grooid-shell-example.
    前のコミットのメッセージは誤爆です
    そのような事実はございません

--- app/src/main/java/com/nao20010128nao/CSoA/MainActivity.groovy
+++ app/src/main/java/com/nao20010128nao/CSoA/MainActivity.groovy
@@ -3,18 +3,20 @@ package com.nao20010128nao.CSoA;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle
 import android.widget.TextView
-import groovy.transform.CompileStatic;
+import groovy.transform.CompileStatic
+import me.champeau.groovydroid.GrooidShell;
 
 public class MainActivity extends AppCompatActivity {
-
+    GrooidShell compiler
     @Override
     @CompileStatic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
+        compiler=new GrooidShell(filesDir,classLoader)
         findViewById(R.id.run).onClickListener={
             def text=(findViewById(R.id.text) as TextView).text
-            def output=new ConfigSlurper().parse(text.toString())
+            def output=new ConfigSlurper().parse(compiler.evaluate(text.toString()).script)
             (findViewById(R.id.result) as TextView).text=output.toString()
         }
     }
